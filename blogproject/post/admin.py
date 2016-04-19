415099eae8e55e0d4ff83c1c415fbdd221dcb320
Damian Maksymiuk <Damian Maksymiuk>
2016-04-19 12:58:59 +0200

    wprowadzenie funkcjonalsnoci: filtrowanie postów, data utworzenia, data aktualizacji, wyszukiwanie po tytule i zawartosci

--- blogproject/post/admin.py
+++ blogproject/post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+class PostModelAdmin(admin.ModelAdmin):
+	list_display =["__unicode__","updated","timestamp"]
+	search_fields = ['content','title']
+	list_filter = ["timestamp"]
+	class Meta:
+		model=Post
+admin.site.register(Post, PostModelAdmin)
